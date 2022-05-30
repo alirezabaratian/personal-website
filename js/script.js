c264d9b6ced445c3a12632305f96d0661ad99df5
alirezabaratian <92058984+alirezabaratian@users.noreply.github.com>
2022-05-30 09:44:12 +0430

    Clean CSS and Optimize JS

--- js/script.js
+++ js/script.js
@@ -1,24 +1,28 @@
-setInterval(function () {
-  let days = document.getElementById("days");
-  let hours = document.getElementById("hours");
-  let minutes = document.getElementById("minutes");
-  let seconds = document.getElementById("seconds");
-  let timer = document.querySelectorAll(".timer-data h1");
+function countdown() {
+  setInterval(function () {
+    let days = document.getElementById("days");
+    let hours = document.getElementById("hours");
+    let minutes = document.getElementById("minutes");
+    let seconds = document.getElementById("seconds");
+    let timer = document.querySelectorAll(".timer-data h1");
+  
+    const launchDate = new Date(2022, 5, 6, 9, 0, 0, 0);
+    const now = new Date();
+    const distance = launchDate - now;
+  
+    if (distance <= 0) {
+      timer.forEach((number) => {
+        number.innerHTML = "00";
+      });
+    } else {
+      days.innerHTML = Math.floor(distance / (1000 * 60 * 60 * 24));
+      hours.innerHTML = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      minutes.innerHTML = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      seconds.innerHTML = Math.floor((distance % (1000 * 60)) / 1000);
+    }
+  }, 1000);
+}
 
-  const launchDate = new Date(2022, 5, 6, 9, 0, 0, 0);
-  const now = new Date();
-  const distance = launchDate - now;
-
-  if (distance <= 0) {
-    timer.forEach((number) => {
-      number.innerHTML = "00";
-    });
-  } else {
-    days.innerHTML = Math.floor(distance / (1000 * 60 * 60 * 24));
-    hours.innerHTML = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    minutes.innerHTML = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    seconds.innerHTML = Math.floor((distance % (1000 * 60)) / 1000);
-  }
-}, 1000);
+countdown();
