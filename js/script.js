afb586e794e6f986316718c4a861f2b75f3b2062
alirezabaratian <92058984+alirezabaratian@users.noreply.github.com>
2022-06-06 13:53:14 +0430

    Launch date is pushed back:(

--- js/script.js
+++ js/script.js
@@ -6,13 +6,13 @@ function countdown() {
     let seconds = document.getElementById("seconds");
     let timer = document.querySelectorAll(".timer-data h1");
   
-    const launchDate = new Date(2022, 5, 6, 9, 0, 0, 0);
+    const launchDate = new Date(2022, 5, 12, 9, 0, 0, 0);
     const now = new Date();
     const distance = launchDate - now;
   
     if (distance <= 0) {
       timer.forEach((number) => {
-        number.innerHTML = "00";
+        number.innerHTML = "0";
       });
     } else {
       days.innerHTML = Math.floor(distance / (1000 * 60 * 60 * 24));
