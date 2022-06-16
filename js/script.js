c8ced747715268a34bd69fb0b32e12afcd8db227
alirezabaratian <92058984+alirezabaratian@users.noreply.github.com>
2022-06-16 18:59:04 +0430

    [Update] Launch Date

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function countdown() {
     let seconds = document.getElementById("seconds");
     let timer = document.querySelectorAll(".timer-data h1");
   
-    const launchDate = new Date(2022, 5, 18, 9, 0, 0, 0);
+    const launchDate = new Date(2022, 5, 20, 9, 0, 0, 0);
     const now = new Date();
     const distance = launchDate - now;
   
